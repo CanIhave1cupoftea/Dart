4229723ac607c97247138570571cdb82af97bd64
CanIhave1cupoftea <codergahds@gmail.com>
2023-12-02 17:35:14 +0800

    just made some variables

--- helloworld.dart
+++ helloworld.dart
@@ -1,3 +1,20 @@
 void main(List<String> args) {
   print("Hello World");
+
+  var firstVar = "I am the first variable initialized"; //var is used for initializing variable of unsure type
+  var firstObject = "I am the first Object to be Initialized";
+
+  print(firstVar);
+
+  String name = "Henry";
+  int age = 21;
+  const birthDay = "March 08, 2002";
+  final faveSong = "When She Cries - Restless Heart";
+  print(name);
+  print(age);
+  print(birthDay);
+  print(faveSong);
+
+
+
 }
